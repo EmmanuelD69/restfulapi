814180177cc60fcc05418bd5f4d2e698e602e8fa
EmmanuelD69 <emmanueldurouge@yahoo.com>
2020-10-02 14:50:37 +0200

    Step8: introducing dotenv to hide sensitive infos -creation of .env file -transering db connexion infos to .env -modifying code on app.js to get db connexion to work with .env file

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 /* loading packages we will use in this project */
 const express = require("express");
-
+const mongoose = require("mongoose");
+require("dotenv/config");
 /* variable to execute express package */
 const app = express();
 
@@ -9,5 +10,11 @@ app.get("/", (req, res) => {
   res.send("we are on homepage!");
 });
 
+/* connecting to DB */
+mongoose.connect(
+  process.env.DB_CONNECT,
+  { useUnifiedTopology: true, useNewUrlParser: true },
+  () => console.log("connected to the database!")
+);
 /* Server settings */
 app.listen(3000, () => console.log("Server is up and running!"));
