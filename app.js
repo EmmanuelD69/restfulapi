86091f8e24cde4ea3f6c7cef3bac92f756e87b7d
EmmanuelD69 <emmanueldurouge@yahoo.com>
2020-10-02 14:18:19 +0200

    Step7: Connecting to Database -loading mongoose to the project -setting connexion to MongoDB (need to create a DB on mongodb.com) -Updating connexion with mongodb server DepecationWarning messages

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 /* loading packages we will use in this project */
 const express = require("express");
+const mongoose = require("mongoose");
 
 /* variable to execute express package */
 const app = express();
@@ -9,5 +10,11 @@ app.get("/", (req, res) => {
   res.send("we are on homepage!");
 });
 
+/* connecting to DB */
+mongoose.connect(
+  "mongodb+srv://EmmanuelDev:testdb@emmanueldevdb0.ztbcz.mongodb.net/EmmanuelDevDB0?retryWrites=true&w=majority",
+  { useUnifiedTopology: true, useNewUrlParser: true },
+  () => console.log("connected to the database!")
+);
 /* Server settings */
 app.listen(3000, () => console.log("Server is up and running!"));
